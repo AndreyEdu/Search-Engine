704ad4b257acdb6b696b65f4b9eb8fc0c4f51e09
Andrey <120014414+ArmyAlc@users.noreply.github.com>
2022-12-20 11:28:04 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,19 +4,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Enter the number of people:");
+
         Scanner scanner = new Scanner(System.in);
-        String[] input = scanner.nextLine().split(" ");
-        String find = scanner.next();
-        int countWord = 0;
-        for (int i = 0; i < input.length; i++) {
-            if (input[i].equals(find)) {
-                countWord = i + 1;
+        int numberOfPeople = Integer.parseInt(scanner.nextLine());
+        String[] dataOfPeople = new String[numberOfPeople];
+
+        System.out.println("Enter all people:");
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            dataOfPeople[i] = scanner.nextLine();
+        }
+
+        System.out.println("Enter the number of search queries:");
+        int numberOfSearch = scanner.nextInt();
+
+        do {
+            System.out.println("Enter data to search people:");
+
+            String searchData = scanner.next();
+            searchPeople(searchData, dataOfPeople);
+
+            numberOfSearch--;
+
+        } while (numberOfSearch != 0);
+    }
+
+    protected static void searchPeople(String searchData, String[] dataOfPeople) {
+        int noMatch = 0;
+        for (String dataOfPerson : dataOfPeople) {
+            if (dataOfPerson.toLowerCase().contains(searchData.toLowerCase())) {
+                System.out.println(dataOfPerson);
+                noMatch++;
             }
         }
-        if (countWord == 0) {
-            System.out.println("Not found");
-        } else {
-            System.out.println(countWord);
+
+        if (noMatch == 0) {
+            System.out.println("No matching people found.");
         }
     }
 }
